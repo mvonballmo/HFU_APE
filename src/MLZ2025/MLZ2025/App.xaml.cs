e2200e44127fae882e76a1b3278febc88121c221
Marco von Ballmoos <marco@earthli.com>
2025-05-04 15:39:52 +0200

    Add initial Maui project from 2025

---
+++ src/MLZ2025/MLZ2025/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MLZ2025;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ src/MLZ2025/MLZ2025/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MLZ2025;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
