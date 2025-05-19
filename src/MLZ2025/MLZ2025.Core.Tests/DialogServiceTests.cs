ffb62f47306941ee6b191e31cbeb4868b7c3b5c4
Marco von Ballmoos <marco@earthli.com>
2025-05-19 19:46:15 +0200

    Add test for adding an item

--- src/MLZ2025/MLZ2025.Core.Tests/DialogServiceTests.cs
+++ src/MLZ2025/MLZ2025.Core.Tests/DialogServiceTests.cs
@@ -29,6 +29,25 @@ public class DialogServiceTests
         Assert.That(testDialogService.LastMessage, Is.EqualTo("Please enter a text"));
     }
 
+    [Test]
+    public void TestAddItem()
+    {
+        var testDialogService = new TestDialogService();
+        var serviceProvider = new ServiceCollection()
+            .AddCoreServices()
+            .AddSingleton<IDialogService>(testDialogService)
+            .AddSingleton<IConnectivity, TestConnectivity>()
+            .BuildServiceProvider();
+
+        var viewModel = serviceProvider.GetRequiredService<MainViewModel>();
+        viewModel.Text = "Item 1";
+
+        viewModel.AddCommand.Execute(null);
+
+        Assert.That(testDialogService.LastMessage, Is.EqualTo(""));
+        Assert.That(viewModel.Items.Last(), Is.EqualTo("Item 1"));
+    }
+
     private class TestDialogService : IDialogService
     {
         public Task ShowErrorMessage(string message)
@@ -38,6 +57,18 @@ public class DialogServiceTests
             return Task.CompletedTask;
         }
 
-        public string LastMessage { get; set; }
+        public string LastMessage { get; set; } = string.Empty;
+    }
+
+    private class TestConnectivity : IConnectivity
+    {
+        public NetworkAccess NetworkAccess => NetworkAccess.Internet;
+        public event EventHandler<ConnectivityChangedEventArgs>? ConnectivityChanged;
+
+        public IEnumerable<ConnectionProfile> ConnectionProfiles => new List<ConnectionProfile>
+        {
+            ConnectionProfile.WiFi,
+            ConnectionProfile.Cellular
+        };
     }
 }
