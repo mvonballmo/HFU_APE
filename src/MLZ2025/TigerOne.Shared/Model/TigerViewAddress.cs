e85abdee1f951ea397977d8beefc87fdb7516dac
Marco von Ballmoos <marco@earthli.com>
2025-06-23 20:58:55 +0200

    Extract From/To Address methods

--- src/MLZ2025/TigerOne.Shared/Model/TigerViewAddress.cs
+++ src/MLZ2025/TigerOne.Shared/Model/TigerViewAddress.cs
@@ -1,10 +1,37 @@
 using System.ComponentModel.DataAnnotations;
+using MLZ2025.Shared.Model;
 using SQLite;
 
 namespace TigerOne.Shared.Model;
 
 public class TigerViewAddress
 {
+    public static TigerViewAddress FromDatabaseAddress(DatabaseAddress address)
+    {
+        return new TigerViewAddress
+        {
+            FirstName = address.FirstName,
+            LastName = address.LastName,
+            ZipCode = address.ZipCode,
+            Birthday = address.Birthday,
+            Email = address.Email,
+            Phone = address.Phone
+        };
+    }
+
+    public static DatabaseAddress FromTigerViewAddress(TigerViewAddress address)
+    {
+        return new DatabaseAddress
+        {
+            FirstName = address.FirstName,
+            LastName = address.LastName,
+            ZipCode = address.ZipCode,
+            Birthday = address.Birthday,
+            Email = address.Email,
+            Phone = address.Phone
+        };
+    }
+
     [Required]
     public string FirstName { get; set; } = "";
 
